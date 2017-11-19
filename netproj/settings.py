d2f80112a0e8d2a566775af458613cc130d2afaa
srl3630 <samrlipton@gmail.com>
2017-11-18 20:14:37 -0500

    Backend code and database added.

--- netproj/settings.py
+++ netproj/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'mynetprojdb',
 	'USER': 'netproj',
-	'PASSWORD': 'Short)Girl(Covered)',
+	'PASSWORD': 'default',
 	'HOST': '127.0.0.1',
 	'PORT': '5432'
     }
